835e93f4a14db8324cbc447df11533f8f4deb5f6
Kevin Moore <kevmoo@google.com>
2026-09-20 16:34:22 +0000

    Revert "chore: Revert global substrate switch to native"
    This reverts commit dd795ea48f7bfbc36b9532fa0e294dc9bde3225d.

--- pkgs/codable/lib/src/json/substrate/substrate.dart
+++ pkgs/codable/lib/src/json/substrate/substrate.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Active substrate dispatcher (switched to native mode).
+/// Active substrate dispatcher (switched to mock mode).
 library;
 
-export 'substrate_native.dart';
+export 'mock/substrate_mock.dart';
